84d7bc3c5100a9d14972dfef255f8a443902c81a
= <femeuf5@gmail.com>
2022-08-01 17:02:36 -0300

    first commit

---
+++ src/routes/Input.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    let text: string = '';
+</script>
+
+<input type="text" bind:value={text}/>
+<p bind:innerHTML={text} contenteditable="true">dflskj dsfkjsdlkf ldskjfldskj fldsjf</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Input from './Input.svelte';
+
+    let count: any = 0;
+
+    const increment_by_one = () => {
+        count++;
+    }
+</script>
+
+<div class="index">
+    <Input />
+    <h2 on:click={increment_by_one}>Count: <span bind:innerHTML={count} contenteditable="true"></span></h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima unde magnam voluptatum omnis neque, cum, officiis dolor debitis tenetur natus veritatis reiciendis. Modi officia, laboriosam blanditiis exercitationem cupiditate reiciendis officiis.</p>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        margin: 20px auto;
+    }
+</style>
